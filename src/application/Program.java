6c8788166942b6fbff725d38339e297760bd5082
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-17 15:41:30 -0300

    Project upadate - Second demostration.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
-		demoFirst();
+		//demoFirst();
+		demoSecond();
 	}
 
 	public static void showMaps(Map<String, String> cookies) {
@@ -40,4 +44,15 @@ public class Program {
 		System.out.println("Size: " + cookies.size());
 	}
 
+	public static void demoSecond() {
+		Map<Product, Double> stock = new HashMap<>();
+		Product p1 = new Product("Tv", 900.0);
+		Product p2 = new Product("Notebook", 1200.0);
+		Product p3 = new Product("Tablet", 400.0);
+		stock.put(p1, 10000.0);
+		stock.put(p2, 20000.0);
+		stock.put(p3, 15000.0);
+		Product ps = new Product("Tv", 900.0);
+		System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
+	}
 }
